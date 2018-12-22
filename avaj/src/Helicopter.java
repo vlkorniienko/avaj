4e4c4747e62ec945ce693fe9bcdfb6af1699783f
vova91 <v.korniienko91@gmail.com>
2018-12-22 11:35:02 +0200

    added some methods

--- avaj/src/Helicopter.java
+++ avaj/src/Helicopter.java
@@ -2,9 +2,19 @@
  * Created by vkorniie on 15/12/2018.
  */
 public class Helicopter extends Aircraft implements Flyable {
-    //WeatherTower weathertower;
+    WeatherTower weathertower;
 
     protected Helicopter(String name, Coordinates coordinates) {
         super(name, coordinates);
+    }
+
+    @Override
+    public void registerTower(WeatherTower weatherTower) {
+        weatherTower.register(this);
+    }
+
+    @Override
+    public void updateConditions() {
+        
     }
 }
