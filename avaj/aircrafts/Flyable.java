6689dbd5d8523584d6850a74adc8e9ff66d582ab
vova91 <v.korniienko91@gmail.com>
2018-12-26 21:25:52 +0200

    rm

--- avaj/aircrafts/Flyable.java
+++
@@ -1,9 +0,0 @@
-package avaj.aircrafts;
-
-import avaj.aircrafts.*;
-import avaj.weather.*;
-
-public interface Flyable {
-    public void updateConditions ();
-    public void registerTower(WeatherTower weatherTower);
-}
